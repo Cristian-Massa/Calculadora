8fd8543cb557231e9f2047c1a438b5c4049c2d6f
Cristian-Massa <cristianmassa30@gmail.com>
2023-04-19 01:23:59 -0300

    deleted btn 0

--- src/components/Botones/Botones.jsx
+++ src/components/Botones/Botones.jsx
@@ -76,7 +76,6 @@ export const Botones = (props) => {
                 <Buttons value={2} onClick={ingresarOperacion}>2</Buttons>
                 <Buttons value={3} onClick={ingresarOperacion}>3</Buttons>
                 <Buttons value={0} onClick={ingresarOperacion}>0</Buttons>
-                <Buttons value={0} onClick={ingresarOperacion}>0</Buttons>
                 <Buttons value={'.'} onClick={ingresarOperacion}>.</Buttons>
             </ContainerNums>
             <ContainerOperators>
